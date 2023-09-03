49eace14876a46b876052653fc31b79ba73b3e08
LucasRosa53 <lucasrosa223@hotmail.com>
2023-09-03 18:47:30 -0300

    resolvido requisito 6

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,11 @@
-class Report:
-    pass
+from inventory_report.inventory import Inventory
+from typing import Protocol
+
+
+class Report(Protocol):
+    def add_inventory(self, inventory: Inventory) -> None:
+        pass
+    
+
+    def generate(self) -> str:
+        pass
