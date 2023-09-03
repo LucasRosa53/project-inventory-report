b78db04bc1b96e372fb7badb749a7d06e4efad7a
LucasRosa53 <lucasrosa223@hotmail.com>
2023-09-03 18:20:50 -0300

    requisito 4

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,6 +1,7 @@
 from typing import Dict, Type
 from inventory_report.product import Product
 from abc import ABC, abstractmethod
+import json
 
 class Importer(ABC):
     def __init__(self, path: str) -> None:
@@ -13,8 +14,24 @@ class Importer(ABC):
 
 
 class JsonImporter(Importer):
-    pass
+    def import_data(self) -> list[Product]:
+        with open(self.path, "r") as file:
+            data = json.load(file)
 
+        products = []    
+        for item in data:
+            product = Product(
+                item.get("id", ""),
+                item.get("product_name", ""),
+                item.get("company_name", ""),
+                item.get("manufacturing_date", ""),
+                item.get("expiration_date", ""),
+                item.get("serial_number", ""),
+                item.get("storage_instructions", "")
+            )
+            products.append(product)
+
+        return products
 
 class CsvImporter:
     pass
